0f4321bee52a2e8b7af6719a04db96e42dea4049
justin talbott <justin@waymondo.com>
2016-02-24 15:56:55 -0500

    un-nest deep macro expansion, bump version
    fixes #11

--- apropospriate-theme-pkg.el
+++ apropospriate-theme-pkg.el
@@ -1,4 +1,5 @@
 (define-package
   "apropospriate-theme"
-  "0.0.1"
-  "A colorful, low-contrast, light & dark theme set for Emacs with a fun name.")
+  "0.1.0"
+  "A colorful, low-contrast, light & dark theme set for Emacs with a fun name."
+  'nil :url "https://github.com/waymondo/apropospriate-theme" :keywords '("color" "theme"))
