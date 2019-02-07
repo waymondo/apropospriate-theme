7531edcc98ad8246914c79f11634b1ed95afaf05
justin talbott <justin@waymondo.com>
2019-02-07 17:30:35 -0500

    restore green underline for flymake-note
    though it may lead to noisy underlines for large diagnostic regions, a
    visible default is better than none

--- apropospriate.el
+++ apropospriate.el
@@ -284,7 +284,7 @@ Set to `1.0' or nil to prevent font size manipulation."
      `(flycheck-inline-info ((,class (:foreground ,base03 :background ,base00-1 :inherit unspecified))))
      `(flymake-error ((,class (:underline (:style wave :color ,red) :inherit unspecified))))
      `(flymake-warning ((,class (:underline (:style wave :color ,orange) :inherit unspecified))))
-     `(flymake-note ((,class (:underline nil :inherit unspecified))))
+     `(flymake-note ((,class (:underline (:style wave :color ,green) :inherit unspecified))))
      `(flymake-diagnostic-at-point-posframe-background-face ((,class (:background ,base00-1))))
      `(swoop-face-target-words ((,class (:foreground ,yellow))))
      `(swoop-face-target-line ((,class (:background ,base01))))
